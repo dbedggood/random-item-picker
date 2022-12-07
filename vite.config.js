d5b82dd6fd9508255d0e718b06fc058d0b9680ba
Daniel Bedggood <T828767@spark.co.nz>
2022-12-07 17:05:51 +1300

    Fix vite config base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: "random-item-picker",
+  base: "/random-item-picker/",
   build: {
     outDir: "docs",
   },
