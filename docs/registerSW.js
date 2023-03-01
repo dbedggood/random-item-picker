d21423d09943a65d664be7ed0567acdcef2cafd4
Daniel Bedggood <T828767@spark.co.nz>
2023-03-01 14:43:23 +1300

    Add PWA plugin

---
+++ docs/registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/random-item-picker/sw.js', { scope: '/random-item-picker/' })})}
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [VitePWA(), svelte()],
   base: "/random-item-picker/",
   build: {
     outDir: "docs",
