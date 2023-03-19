051a5ce18359d88a401270fdea3ebb815ff29bf0
Daniel Bedggood <ddbedggood@gmail.com>
2023-03-19 14:45:52 +1300

    Add new icons and disable service worker

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,32 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [VitePWA(), svelte()],
+  plugins: [
+    svelte(),
+    VitePWA({
+      selfDestroying: true, // no service worker
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Random Item Picker",
+        short_name: "random-item-picker",
+        description: "An app to help you pick randomly from a list of items.",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/random-item-picker/",
   build: {
     outDir: "docs",
